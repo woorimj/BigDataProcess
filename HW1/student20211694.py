e049cf5fc30c0fd9d018191ef6ad1f4084832f61
woorimj <128919405+woorimj@users.noreply.github.com>
2023-11-03 05:05:18 +0900

    등급 비율 재설정, 엑셀 파일 정렬

--- HW1/student20211694.py
+++ HW1/student20211694.py
@@ -1,42 +1,50 @@
-#!/usr/bin/python3
-
 from openpyxl import load_workbook
 
 wb = load_workbook(filename="student.xlsx")
 sheet = wb.active
 
-students = sheet.max_row - 1  # header값 빼고 총 학생 수 구하기
-aStu = int(students * 0.3)
-bStu = int(students * 0.7)
-bplusStu = int(bStu * 0.5)
-cStu = students - aStu - bStu
+data = list(sheet.iter_rows(values_only=True))
 
-for row in range(2, sheet.max_row + 1):
-    total = sheet.cell(row=row, column=7).value  # total값 계산한거 가져오기
+students = len(data) - 1  # header를 제외한 총 학생 수
 
-    # 40점 미만이면 F학점
-    if total < 40:
+# total 값으로 내림차순 정렬
+data = sorted(data[1:], key=lambda row: row[6], reverse=True)
+
+# 등급 비율계산
+A = int(students * 0.3)
+A_plus = int(students * 0.3 * 1.5)
+B = int(students * 0.7)
+B_plus = int(students * 0.7 * 1.5)
+
+grades = ["A+", "A0", "B+", "B0", "C+", "C0", "F"]
+
+for i in range(len(data)):
+    row = list(data[i])
+    if row[6] < 40:
         grade = "F"
+    elif i < A:
+        grade = grades[0]
+    elif i < A_plus:
+        grade = grades[1]
+    elif i < B:
+        grade = grades[2]
+    elif i < B_plus:
+        grade = grades[3]
+    elif i < students:
+        grade = grades[4]
     else:
-        if aStu > 0:
-            grade = "A+"
-            aStu -= 1
-        elif bplusStu > 0:
-            grade = "A0"
-            if aStu == 0:
-                grade = "B+"
-            bplusStu -= 1
-        elif bStu > 0:
-            grade = "B0"
-            bStu -= 1
-        elif bplusStu > 0:
-            grade = "B+"
-            if bStu == 0:
-                grade = "C+"
-            bplusStu -= 1
-        else:
-            grade = "C"
+        grade = grades[5]
+    row[7] = grade
+    data[i] = tuple(row)
 
-    sheet.cell(row=row, column=8, value=grade)
+# 학번으로 다시 오름차순 정렬
+data = sorted(data, key=lambda row: row[0])
 
+# 엑셀에 데이터 입력
+for i in range(2, len(data) + 2):
+    row_data = data[i - 2]
+    for j in range(1, len(row_data) + 1):
+        sheet.cell(row=i, column=j, value=row_data[j - 1])
+
+# 엑셀 파일 저장
 wb.save("student.xlsx")
