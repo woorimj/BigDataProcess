d0bd684e10d47fc01aa2ba06c6a02691d522a570
woorimj <128919405+woorimj@users.noreply.github.com>
2023-11-09 11:54:19 +0900

    feat:line.strip() 설정다시

--- HW2/IMDBStudent20211694.py
+++ HW2/IMDBStudent20211694.py
@@ -3,14 +3,16 @@
 counts = {}
 with open("movies_exp.txt", "r") as file:
     for line in file:
+        line = line.strip()
         splits = line.split("::")  # '::'으로 세부분으로 나누기 (id, 영화제목, 장르)
         genre_list = splits[2].split("|")  # 영화장르 여러개인 경우 '|'로 나누기
         # # 해당 line의 장르만 추출되어서 저장되어있음(공백, 줄바꿈이 있었음)
+        # print(genre_list)
         # for genre in genre_list:
         #     # genre = genre.strip()
         #     print(genre)
         for genre in genre_list:
-            genre = genre.strip()
+            # genre = genre.strip()
             if genre in counts:
                 counts[genre] += 1
             else:
